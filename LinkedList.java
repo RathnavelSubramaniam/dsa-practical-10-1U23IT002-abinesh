613ad544940da835b37f59f1a92570d7bb05c472
1U23IT002-abinesh <139448037+1U23IT002-abinesh@users.noreply.github.com>
2025-03-06 09:48:59 +0000

    Done10

--- LinkedList.java
+++ LinkedList.java
@@ -3,16 +3,16 @@ public class LinkedList {
     class Node {
     int data;
     Node next;
- 
+
     public Node(int data) {
         this.data = data;
         this.next = null;
         }
     }
- 
+
     Node head = null;
     Node tail = null;
- 
+
 public void addNode(int data) {
         Node newNode = new Node(data);
         if (head == null) {
@@ -23,37 +23,46 @@ public void addNode(int data) {
             tail = newNode;
         }
     }
- 
+
 public static void main(String[] args) {
 LinkedList list = new LinkedList();
 Scanner sc = new Scanner(System.in);
- 
+
 System.out.println("Enter integers to create nodes (enter a non-integer to exit):");
- 
+
 while (sc.hasNextInt()) {
 int data = sc.nextInt();
 list.addNode(data);
         }
 System.out.println("Linked List created");
- 
+
 System.out.println("Linked List Before Deleting");
 list.displayList();
-System.out.println("Deletion at Beginning");
-list.deleteBegin();
-System.out.println("Linked List After Deleting at Beginning");
+System.out.println("Deletion at Tail");
+list.deleteEnd();
+System.out.println("Linked List After Deleting at tail");
 list.displayList();
 sc.close();
     }
-public void deleteBegin()
+public void deleteEnd()
     {
         if(head==null)
-        System.out.println("List is empty");
+System.out.println("List is empty");
+else if(head.next==null)
+        head=null;
         else
         {
             Node temp=head;
-            head=head.next;
-            System.out.println("Deleted Element is:"+temp.data);
-        }
+            Node prev=head;
+            while(temp.next.next!=null)
+            {
+                prev=prev.next;
+                temp=temp.next;
+            }
+            temp=temp.next;
+prev.next=null;
+System.out.println("Deleted Element is:"+temp.data);
+        }        
     }
 public void displayList() {
         Node current = head;
